5304c642f0150e74f322a15bc04d4c8b6563f73f
JulesPomme <julian.moreira.fr@gmail.com>
2022-12-05 21:50:35 +0100

    debug throwing point position update

--- Assets/Scripts/DeclareSelfToScriptableObject.cs
+++ Assets/Scripts/DeclareSelfToScriptableObject.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeclareSelfToScriptableObject : MonoBehaviour
-{
-    public enum ScriptableObjectType
-    {
+public class DeclareSelfToScriptableObject : MonoBehaviour {
+    public enum ScriptableObjectType {
         GAMEOBJECT, POSITION, ROTATION, SCALE
     }
     public ScriptableObjectType type;
     public GameObjectSO gameObjectSO;
     public Vector3SO vector3SO;
+    /// <summary>
+    /// Set to true if you want the value to be updated regularly via the Update function. If false, the value is set once in the Awake function.
+    /// </summary>
+    public bool update = false;
 
     void Awake() {
+        SetScriptableValue();
+    }
+
+    private void Update() {
+        if (update) {
+            SetScriptableValue();
+        }
+    }
+
+    private void SetScriptableValue() {
         switch (type) {
             case ScriptableObjectType.GAMEOBJECT:
                 gameObjectSO.obj = gameObject;
